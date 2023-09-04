216a4808de93b4f2ea06436129e917b4dc01e9cc
vrionides2000 <vrionides2000@gmail.com>
2023-09-04 12:51:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import profilePic from './Profile Picture.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <div className='profile-container'>
+        <img className='profile-picture' src={profilePic} alt="marcos's profile" />
+        <div className='profile-div'>
+          <div className='name'>Marcos Vrionides</div>
+          <div className='occupation'>Software Developer</div>
+        </div>
+      </div>
+      <div className='introduction-container'>
+        <div className='emoji'>👋</div>
+        <div className='intro'>
+          <span className='highlight'>Hey,</span> I’m Marcos a  junior software developer with a passion for building innovative projects and an eagerness to learn and contribute my skills and knowledge to a dynamic team.
+        </div>
+      </div>
+      <div className='projects-container'>
+        <div className='projects-title'>
+          Projects
+        </div>
+        <div className='projects-cards'>
+          <div className='project'>
+            <div className='project-title'>My Lists</div>
+          </div>
+          <div className='project'>
+            <div className='project-title'>StudentsThoughts.com</div>
+          </div>
+          <div className='project'>
+            <div className='project-title'>Connect</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
